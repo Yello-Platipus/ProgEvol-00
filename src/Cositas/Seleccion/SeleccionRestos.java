f90d8e92d3784531c73db6e77e97aa885ef60862
Yello-Platipus <danieltinez1@gmail.com>
2023-03-02 19:40:07 +0100

    Cositas-06-Ayuda me muero

--- src/Cositas/Seleccion/SeleccionRestos.java
+++ src/Cositas/Seleccion/SeleccionRestos.java
@@ -7,11 +7,24 @@ import java.util.ArrayList;
 public class SeleccionRestos extends Seleccion{
     @Override
     public String toString() {
-        return null;
+        return "Seleccion por restos";
     }
 
     @Override
     public ArrayList<Individuo> seleccionar(ArrayList<Individuo> poblacion, int tamTorneo) {
-        return null;
+        int tamPoblacion = poblacion.size();
+        double fitnessTotal = 0;
+        ArrayList<Individuo> seleccionados = new ArrayList<>();
+        for(Individuo i : poblacion)
+            fitnessTotal += i.getFitness();
+
+        for (int i = 0; i < tamPoblacion; i++) {
+            if(tamPoblacion * (poblacion.get(i).getFitness() / fitnessTotal) > 1)
+                seleccionados.add(poblacion.get(i));
+        }
+        while(seleccionados.size() < tamPoblacion) // El resto se selecciona de manera aleatoria
+            seleccionados.add(poblacion.get((int) (Math.random() * tamPoblacion)));
+
+        return seleccionados;
     }
 }
